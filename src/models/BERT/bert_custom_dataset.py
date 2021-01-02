e9fd54841dc59d0b33fc984560b3001a39e90571
prestrepoh <prestrepoh@gmail.com>
2021-01-02 12:25:38 +0000

    refactored script to train BERT model

---
+++ src/models/BERT/bert_custom_dataset.py
@@ -0,0 +1,40 @@
+import torch
+from torch.utils.data import Dataset, DataLoader, RandomSampler, SequentialSampler
+
+class BERTCustomDataset(Dataset):
+
+    def __init__(self, dataframe, tokenizer, max_len):
+        self.tokenizer = tokenizer
+        self.data = dataframe
+        self.comment_text = dataframe.Conversation
+        self.targets = self.data.list
+        self.max_len = max_len
+
+    def __len__(self):
+        return len(self.comment_text)
+
+    def __getitem__(self, index):
+        
+        comment_text = str(self.comment_text[index])
+        #comment_text = " ".join(comment_text.split())
+
+        inputs = self.tokenizer.encode_plus(
+            comment_text,
+            None,
+            add_special_tokens=True,
+            max_length=self.max_len,
+          	padding='max_length',
+            return_token_type_ids=True,
+            truncation = True
+        )
+        ids = inputs['input_ids']
+        mask = inputs['attention_mask']
+        token_type_ids = inputs["token_type_ids"]
+
+
+        return {
+            'ids': torch.tensor(ids, dtype=torch.long),
+            'mask': torch.tensor(mask, dtype=torch.long),
+            'token_type_ids': torch.tensor(token_type_ids, dtype=torch.long),
+            'targets': torch.tensor(self.targets[index], dtype=torch.float)
+        }
---
+++ src/models/BERT/my_bert_model.py
@@ -0,0 +1,101 @@
+from model_evaluator import ModelEvaluator
+
+import pytorch_lightning as pl
+from transformers import BertTokenizer, BertModel, BertConfig
+import torch
+from torch.utils.data import Dataset, DataLoader, RandomSampler, SequentialSampler
+import torch.nn as nn
+import transformers
+import numpy as np
+
+class MyBERTModel(pl.LightningModule):
+
+    def __init__(self, hparams, training_dataset, validation_dataset, labels, model_to_use):
+        super().__init__()
+
+        self.hparams = hparams
+        self.training_dataset = training_dataset
+        self.validation_dataset = validation_dataset
+        self.labels = labels
+
+        self.l1 = transformers.BertModel.from_pretrained(model_to_use)
+        self.l2 = torch.nn.Dropout(0.3)
+        self.l3 = torch.nn.Linear(768, len(self.labels))
+
+    def forward(self, ids, mask, token_type_ids):
+        _, output_1= self.l1(ids, attention_mask = mask, token_type_ids = token_type_ids, return_dict=False)
+        output_2 = self.l2(output_1)
+        output = self.l3(output_2)
+        return output
+
+    def loss_fn(self, outputs, targets):
+      return torch.nn.BCEWithLogitsLoss()(outputs, targets)
+
+    def general_step(self, batch, batch_idx, mode):
+      ids = batch['ids']
+      mask = batch['mask']
+      token_type_ids = batch['token_type_ids']
+      targets = batch['targets']
+
+      outputs = self.forward(ids, mask, token_type_ids)
+
+      return {'outputs': outputs, 'targets': targets}
+
+    #******Training******
+    #This method runs on each GPU
+    def training_step(self, batch, batch_idx):
+      return self.general_step(batch, batch_idx, "train")
+    
+    #This method aggregates the results of training_step in the different GPUs
+    def training_step_end(self, aggregated_outputs):
+      loss = self.loss_fn(aggregated_outputs["outputs"], aggregated_outputs["targets"])
+      self.log('training_loss',loss)
+      return {'loss':loss}
+    
+    #This method runs at the end of each epoch
+    def training_epoch_end(self, results_of_each_batch):
+      pass
+
+    #******Validation******
+    #This method runs in each GPU
+    def validation_step(self, batch, batch_idx):
+      return self.general_step(batch, batch_idx, "val")
+
+    #This method aggregates the results of validation_step in the different GPUs
+    def validation_step_end (self, aggregated_outputs):
+      outputs = torch.sigmoid(aggregated_outputs['outputs']).cpu().detach().numpy().tolist()
+      predictions = (np.array(outputs) >= 0.5).astype(int)
+
+      targets = aggregated_outputs['targets'].cpu().detach().numpy()
+
+      return {'predictions': predictions, 'targets': targets}
+
+    #This method runs at the end of each epoch
+    def validation_epoch_end(self, results_of_each_batch):
+      predictions = np.empty([0,len(self.labels)])
+      targets = np.empty([0,len(self.labels)])
+
+      for result in results_of_each_batch:
+        predictions = np.concatenate((predictions,result['predictions']))
+        targets = np.concatenate((targets,result['targets']))
+      
+      self.evaluate_results(predictions, targets)
+
+    def evaluate_results(self, predictions, targets):
+      #binary relevance
+      total_accuracy = ModelEvaluator.get_total_accuracy(targets, predictions)
+      self.log('total_accuracy', total_accuracy)
+
+      #one vs rest
+      accuracy_per_label = ModelEvaluator.get_accuracy_per_label(self.labels,targets,predictions)
+      self.log('accuracy_per_label',accuracy_per_label)
+
+    def configure_optimizers(self):
+      return torch.optim.Adam(params = self.parameters(), lr=self.hparams["learning_rate"])
+
+    #******Dataloaders******
+    def train_dataloader(self):
+      return DataLoader(self.training_dataset, batch_size=self.hparams["train_batch_size"], shuffle= self.hparams["shuffle"])
+
+    def val_dataloader(self):
+      return DataLoader(self.validation_dataset, batch_size=self.hparams["validation_batch_size"], shuffle= False)
